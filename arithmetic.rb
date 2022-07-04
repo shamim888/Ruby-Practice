c6fd49fb1e083e53aefcb9c182862fff950e1aa0
shamim888 <shamimnw99@gmail.com>
2022-07-04 17:00:53 +0600

    update

--- arithmetic.rb
+++ arithmetic.rb
@@ -7,3 +7,27 @@ end
 sum(1,2,3,4);
 
 sum('billy', ' boy', ' loves', ' toy');
+
+#==============swap function===============
+
+x= 43;
+y=29;
+
+print "#{x} #{y} \n"
+
+x,y = y,x;
+
+print "#{x} #{y}"
+
+=begin
+casting & conveting
+=end
+
+puts 3.14.to_i
+
+puts 45.to_f
+
+puts 34.to_s
+
+puts 250 + '50.34'.to_i
+puts 100 + '34.67'.to_f
