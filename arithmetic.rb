d1b58517fbc6aebc40ba9c96ee8891ef58e60cab
shamim888 <shamimnw99@gmail.com>
2022-07-05 15:34:15 +0600

    3rd commit

--- arithmetic.rb
+++ arithmetic.rb
@@ -1,3 +1,41 @@
+#==============numbers=================
+
+puts 35 + 75
+
+puts 7 * 8
+
+puts 13 ** 2
+
+puts 46 % 5
+
+puts 12 + 8 * 3
+
+puts (12 + 8) * 3
+
+puts 13 / 3
+
+#===============increments===============
+
+a = 20
+
+a += 100
+
+puts "The incremented number is: #{a}"
+
+
+num = -40.7
+
+puts num.abs()
+
+puts num.round()
+
+
+
+puts Math.sqrt(169)
+
+puts Math.log(0)
+
+
 # =============add numbers=============
 
 def sum(a,b,c,d)
@@ -17,13 +55,13 @@ print "#{x} #{y} \n"
 
 x,y = y,x;
 
-print "#{x} #{y}"
+print "#{x} #{y} \n"
 
 =begin
 casting & conveting
 =end
 
-puts 3.14.to_i
+puts 9.82.to_i
 
 puts 45.to_f
 
@@ -31,3 +69,13 @@ puts 34.to_s
 
 puts 250 + '50.34'.to_i
 puts 100 + '34.67'.to_f
+
+#=============== string length, slice, splice Methods ====================
+
+greeting = "hello, friend!"
+
+puts greeting.length;
+puts greeting[5]
+puts greeting.include? "fri"
+puts greeting.include? "com"
+puts greeting[5,8]
